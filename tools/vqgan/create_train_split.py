208ae564a2b578fe8cc69c4f3b83d9b4845db6d7
Tundra Work <tundrawork@gmail.com>
2023-12-19 22:04:51 +1100

    feat(tools): vqgan/create_train_split: support using --val_ratio and --val_count to adjust split

--- tools/vqgan/create_train_split.py
+++ tools/vqgan/create_train_split.py
@@ -10,7 +10,9 @@ from fish_speech.utils.file import AUDIO_EXTENSIONS, list_files
 
 @click.command()
 @click.argument("root", type=click.Path(exists=True, path_type=Path))
-def main(root):
+@click.option("val_ratio", type=float, default=0.2)
+@click.option("val_count", type=int, default=None)
+def main(root, val_ratio, val_count):
     files = list_files(root, AUDIO_EXTENSIONS, recursive=True)
     print(f"Found {len(files)} files")
 
@@ -21,10 +23,10 @@ def main(root):
     # use 3:1 ratio on train vs val
     val_size = math.ceil(len(files) / 4)  # at least one val file
 
-    with open(root / "vq_train_filelist.txt", "w") as f:
+    with open(root / "vq_train_filelist.txt", "w", encoding="utf-8") as f:
         f.write("\n".join(files[val_size:]))
 
-    with open(root / "vq_val_filelist.txt", "w") as f:
+    with open(root / "vq_val_filelist.txt", "w", encoding="utf-8") as f:
         f.write("\n".join(files[:val_size]))
 
     print("Done")
