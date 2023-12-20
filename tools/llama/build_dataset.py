37ff27295262b86faad4140b53e642c63dfba7ee
Stardust·减 <2225664821@qq.com>
2023-12-20 13:51:01 +0800

    Add files via upload

--- tools/llama/build_dataset.py
+++ tools/llama/build_dataset.py
@@ -3,7 +3,6 @@ import re
 from collections import defaultdict
 from multiprocessing import Pool
 from pathlib import Path
-
 import click
 import numpy as np
 import yaml
@@ -16,7 +15,7 @@ from fish_speech.text import g2p
 from fish_speech.utils.file import AUDIO_EXTENSIONS, list_files
 
 
-def task_generator(config, filelist):
+def task_generator(config,filelist):
     with open(config, "r") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
@@ -32,7 +31,7 @@ def task_generator(config, filelist):
         # Load the files
         if filelist:
             with open(filelist, "r", encoding="utf-8") as f:
-                # files = [Path(line..strip().split("|")[0]) for line in f]
+                #files = [Path(line..strip().split("|")[0]) for line in f]
                 files = set()
                 countSame = 0
                 countNotFound = 0
@@ -43,7 +42,7 @@ def task_generator(config, filelist):
                         countSame += 1
                         continue
                     if not os.path.isfile(file):
-                        # 过滤数据集错误：不存在对应音频
+                    # 过滤数据集错误：不存在对应音频
                         print(f"没有找到对应的音频：{file}")
                         countNotFound += 1
                         continue
@@ -59,26 +58,24 @@ def task_generator(config, filelist):
                 p = file.parent.parent.name
             else:
                 raise ValueError(f"Invalid parent level {parent_level}")
-
             grouped_files[p].append(file)
 
         logger.info(f"Found {len(grouped_files)} groups in {root}")
         for name, subset in grouped_files.items():
-            yield name, subset, source, languages, extension
+            yield name, subset, source, languages, extension, filelist
 
 
 def run_task(task):
-    name, subset, source, languages, extension = task
+    name, subset, source, languages, extension, filelist = task
 
     # Parse the files
     sentences = []
     for file in subset:
         np_file = file.with_suffix(".npy")
         txt_file = file.with_suffix(extension)
-        if np_file.exists() is False or txt_file.exists() is False:
+        if np_file.exists() is False or txt_file.exists() and filelist is False:
             logger.warning(f"Can't find {np_file} or {txt_file}")
             continue
-
         with open(txt_file, "r") as f:
             text = f.read().strip()
 
@@ -122,12 +119,11 @@ def run_task(task):
 )
 @click.option("--output", type=click.Path(), default="data/quantized-dataset-ft.protos")
 @click.option("--filelist", type=click.Path(), default=None)
-def main(config, output, filelist):
+@click.option("--num_worker", type=int, default=16)
+def main(config, output, filelist,num_worker):
     dataset_fp = open(output, "wb")
-    with Pool(16) as p:
-        for result in tqdm(
-            p.imap_unordered(run_task, task_generator(config, filelist))
-        ):
+    with Pool(num_worker) as p:
+        for result in tqdm(p.imap_unordered(run_task, task_generator(config, filelist))):
             dataset_fp.write(result)
 
     dataset_fp.close()
