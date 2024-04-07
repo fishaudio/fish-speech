16f91ae5e348e001621e4af02b7f64bfa50cc329
Lengyue <lengyue@lengyue.me>
2024-04-07 08:42:08 +0000

    Implement shallow reflow

--- fish_speech/models/vqgan/modules/wavenet.py
+++ fish_speech/models/vqgan/modules/wavenet.py
@@ -1,4 +1,5 @@
 import math
+from typing import Optional
 
 import torch
 import torch.nn.functional as F
@@ -83,7 +84,14 @@ class ConvNorm(nn.Module):
 class ResidualBlock(nn.Module):
     """Residual Block"""
 
-    def __init__(self, d_encoder, residual_channels, use_linear_bias=False, dilation=1):
+    def __init__(
+        self,
+        residual_channels,
+        use_linear_bias=False,
+        dilation=1,
+        has_condition=True,
+        condition_channels=None,
+    ):
         super(ResidualBlock, self).__init__()
         self.conv_layer = ConvNorm(
             residual_channels,
@@ -93,23 +101,31 @@ class ResidualBlock(nn.Module):
             padding=dilation,
             dilation=dilation,
         )
-        self.diffusion_projection = LinearNorm(
-            residual_channels, residual_channels, use_linear_bias
-        )
-        self.conditioner_projection = ConvNorm(
-            d_encoder, 2 * residual_channels, kernel_size=1
-        )
+
+        if has_condition:
+            self.diffusion_projection = LinearNorm(
+                residual_channels, residual_channels, use_linear_bias
+            )
+            self.condition_projection = ConvNorm(
+                condition_channels, 2 * residual_channels, kernel_size=1
+            )
+
         self.output_projection = ConvNorm(
             residual_channels, 2 * residual_channels, kernel_size=1
         )
 
-    def forward(self, x, conditioner, diffusion_step):
-        diffusion_step = self.diffusion_projection(diffusion_step).unsqueeze(-1)
-        conditioner = self.conditioner_projection(conditioner)
+    def forward(self, x, condition=None, diffusion_step=None):
+        y = x
 
-        y = x + diffusion_step
+        if diffusion_step is not None:
+            diffusion_step = self.diffusion_projection(diffusion_step).unsqueeze(-1)
+            y = y + diffusion_step
 
-        y = self.conv_layer(y) + conditioner
+        y = self.conv_layer(y)
+
+        if condition is not None:
+            condition = self.condition_projection(condition)
+            y = y + condition
 
         gate, filter = torch.chunk(y, 2, dim=1)
         y = torch.sigmoid(gate) * torch.tanh(filter)
@@ -120,117 +136,90 @@ class ResidualBlock(nn.Module):
         return (x + residual) / math.sqrt(2.0), skip
 
 
-class SpectrogramUpsampler(nn.Module):
-    def __init__(self, hop_size):
-        super().__init__()
-
-        if hop_size == 256:
-            self.conv1 = nn.ConvTranspose2d(
-                1, 1, [3, 32], stride=[1, 16], padding=[1, 8]
-            )
-        elif hop_size == 512:
-            self.conv1 = nn.ConvTranspose2d(
-                1, 1, [3, 64], stride=[1, 32], padding=[1, 16]
-            )
-        else:
-            raise ValueError(f"Unsupported hop_size: {hop_size}")
-
-        self.conv2 = nn.ConvTranspose2d(1, 1, [3, 32], stride=[1, 16], padding=[1, 8])
-
-    def forward(self, x):
-        x = torch.unsqueeze(x, 1)
-        x = self.conv1(x)
-        x = F.leaky_relu(x, 0.4)
-        x = self.conv2(x)
-        x = F.leaky_relu(x, 0.4)
-        x = torch.squeeze(x, 1)
-
-        return x
-
-
 class WaveNet(nn.Module):
-    """
-    WaveNet
-    https://www.deepmind.com/blog/wavenet-a-generative-model-for-raw-audio
-    """
-
     def __init__(
         self,
-        mel_channels=128,
-        d_encoder=256,
-        residual_channels=512,
-        residual_layers=20,
-        use_linear_bias=False,
-        dilation_cycle=None,
+        input_channels: Optional[int] = None,
+        output_channels: Optional[int] = None,
+        residual_channels: int = 512,
+        residual_layers: int = 20,
+        dilation_cycle: Optional[int] = 4,
+        is_diffusion: bool = False,
+        condition_channels: Optional[int] = None,
     ):
-        super(WaveNet, self).__init__()
+        super().__init__()
 
-        self.input_projection = ConvNorm(mel_channels, residual_channels, kernel_size=1)
-        self.diffusion_embedding = DiffusionEmbedding(residual_channels)
-        self.mlp = nn.Sequential(
-            LinearNorm(residual_channels, residual_channels * 4, use_linear_bias),
-            Mish(),
-            LinearNorm(residual_channels * 4, residual_channels, use_linear_bias),
-        )
+        # Input projection
+        self.input_projection = None
+        if input_channels is not None and input_channels != residual_channels:
+            self.input_projection = ConvNorm(
+                input_channels, residual_channels, kernel_size=1
+            )
+
+        if input_channels is None:
+            input_channels = residual_channels
+
+        # Residual layers
         self.residual_layers = nn.ModuleList(
             [
                 ResidualBlock(
-                    d_encoder,
-                    residual_channels,
-                    use_linear_bias=use_linear_bias,
+                    residual_channels=residual_channels,
+                    use_linear_bias=False,
                     dilation=2 ** (i % dilation_cycle) if dilation_cycle else 1,
+                    has_condition=is_diffusion,
+                    condition_channels=condition_channels,
                 )
                 for i in range(residual_layers)
             ]
         )
+
+        # Skip projection
         self.skip_projection = ConvNorm(
             residual_channels, residual_channels, kernel_size=1
         )
-        self.output_projection = ConvNorm(
-            residual_channels, mel_channels, kernel_size=1
-        )
-        nn.init.zeros_(self.output_projection.conv.weight)
 
-    def forward(self, x, diffusion_step, conditioner, x_masks=None, cond_masks=None):
-        """
+        # Output projection
+        self.output_projection = None
+        if output_channels is not None and output_channels != residual_channels:
+            self.output_projection = ConvNorm(
+                residual_channels, output_channels, kernel_size=1
+            )
 
-        :param x: [B, M, T]
-        :param diffusion_step: [B,]
-        :param conditioner: [B, M, T]
-        :return:
-        """
+        if is_diffusion:
+            self.diffusion_embedding = DiffusionEmbedding(residual_channels)
+            self.mlp = nn.Sequential(
+                LinearNorm(residual_channels, residual_channels * 4, False),
+                Mish(),
+                LinearNorm(residual_channels * 4, residual_channels, False),
+            )
 
-        # To keep compatibility with DiffSVC, [B, 1, M, T]
-        use_4_dim = False
-        if x.dim() == 4:
-            x = x[:, 0]
-            use_4_dim = True
+        self.apply(self._init_weights)
 
-        assert x.dim() == 3, f"mel must be 3 dim tensor, but got {x.dim()}"
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv1d, nn.Linear)):
+            nn.init.trunc_normal_(m.weight, std=0.02)
+            if getattr(m, "bias", None) is not None:
+                nn.init.constant_(m.bias, 0)
 
-        x = self.input_projection(x)  # x [B, residual_channel, T]
-        x = F.relu(x)
+    def forward(self, x, t=None, condition=None):
+        if self.input_projection is not None:
+            x = self.input_projection(x)
+            x = F.silu(x)
 
-        diffusion_step = self.diffusion_embedding(diffusion_step)
-        diffusion_step = self.mlp(diffusion_step)
-
-        if x_masks is not None:
-            x = x * x_masks
-
-        if cond_masks is not None:
-            conditioner = conditioner * cond_masks
+        if t is not None:
+            t = self.diffusion_embedding(t)
+            t = self.mlp(t)
 
         skip = []
         for layer in self.residual_layers:
-            x, skip_connection = layer(x, conditioner, diffusion_step)
+            x, skip_connection = layer(x, condition, t)
             skip.append(skip_connection)
 
         x = torch.sum(torch.stack(skip), dim=0) / math.sqrt(len(self.residual_layers))
         x = self.skip_projection(x)
-        x = F.relu(x)
-        x = self.output_projection(x)  # [B, 128, T]
 
-        if x_masks is not None:
-            x = x * x_masks
+        if self.output_projection is not None:
+            x = F.silu(x)
+            x = self.output_projection(x)
 
-        return x[:, None] if use_4_dim else x
+        return x
