929ab4cd314c4add2d11cbb385131762a79e0866
Lengyue <lengyue@lengyue.me>
2024-09-09 22:13:03 -0400

    fix license

--- tools/webui.py
+++ tools/webui.py
@@ -42,7 +42,7 @@ HEADER_MD = f"""# Fish Speech
 
 {i18n("You can find the source code [here](https://github.com/fishaudio/fish-speech) and models [here](https://huggingface.co/fishaudio/fish-speech-1).")}  
 
-{i18n("Related code are released under BSD-3-Clause License, and weights are released under CC BY-NC-SA 4.0 License.")}  
+{i18n("Related code and weights are released under CC BY-NC-SA 4.0 License.")}  
 
 {i18n("We are not responsible for any misuse of the model, please consider your local laws and regulations before using it.")}  
 """
