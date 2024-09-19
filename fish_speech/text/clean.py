711209e541953b5ed33c71db5e21642885cacd0b
Lengyue <lengyue@lengyue.me>
2024-09-19 06:02:13 -0400

    update clean text for new model

--- fish_speech/text/clean.py
+++ fish_speech/text/clean.py
@@ -1,7 +1,7 @@
 import re
 
 SYMBOLS_MAPPING = {
-    "\n": ".",
+    "\n": "",
     "…": ".",
     "“": "'",
     "”": "'",
@@ -35,11 +35,28 @@ REPLACE_SYMBOL_REGEX = re.compile(
 )
 
 
+EMOJI_REGEX = re.compile(
+    "["
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "]+",
+    flags=re.UNICODE,
+)
+
+
 def clean_text(text):
     # Clean the text
     text = text.strip()
 
     # Replace all chinese symbols with their english counterparts
     text = REPLACE_SYMBOL_REGEX.sub(lambda x: SYMBOLS_MAPPING[x.group()], text)
+
+    # Remove emojis
+    text = EMOJI_REGEX.sub(r"", text)
+
+    # Remove continuous periods (...) and commas (,,,)
+    text = re.sub(r"[.,]{2,}", lambda m: m.group()[0], text)
 
     return text
