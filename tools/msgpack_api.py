44ea0614e036668080253edecf9b03a03bbc5caa
Picus303 <pilleurc@protonmail.com>
2024-12-03 22:26:30 +0100

    remove unused function

--- tools/msgpack_api.py
+++ tools/msgpack_api.py
@@ -5,43 +5,7 @@ from pathlib import Path
 import httpx
 import ormsgpack
 
-from tools.schema import ServeReferenceAudio, ServeTTSRequest
-
 api_key = os.environ.get("FISH_API_KEY", "YOUR_API_KEY")
-
-
-def audio_request():
-    # priority: ref_id > references
-    request = ServeTTSRequest(
-        text="你说的对, 但是原神是一款由米哈游自主研发的开放世界手游.",
-        # reference_id="114514",
-        references=[
-            ServeReferenceAudio(
-                audio=open("lengyue.wav", "rb").read(),
-                text=open("lengyue.lab", "r", encoding="utf-8").read(),
-            )
-        ],
-        streaming=True,
-    )
-
-    api_key = os.environ.get("FISH_API_KEY", "YOUR_API_KEY")
-
-    with (
-        httpx.Client() as client,
-        open("hello.wav", "wb") as f,
-    ):
-        with client.stream(
-            "POST",
-            "http://127.0.0.1:8080/v1/tts",
-            content=ormsgpack.packb(request, option=ormsgpack.OPT_SERIALIZE_PYDANTIC),
-            headers={
-                "authorization": f"Bearer {api_key}",
-                "content-type": "application/msgpack",
-            },
-            timeout=None,
-        ) as response:
-            for chunk in response.iter_bytes():
-                f.write(chunk)
 
 
 def asr_request(audio_path: Path):
